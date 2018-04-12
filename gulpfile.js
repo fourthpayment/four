bc22492ea6f638090f984bbd2422a9411b460fd3
twilroad <269044570@@qq.com>
2018-04-12 16:04:25 +0800

    chore(compile): add configurations for compile

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+const gulp = require("gulp");
+const rename = require("gulp-rename");
+const sequence = require("gulp-sequence");
+const ts = require("@notadd/gulp-typescript");
+const tslint = require("gulp-tslint");
+
+const packages = {
+    core: ts.createProject("src/core/tsconfig.json"),
+    graphql: ts.createProject("src/graphql/tsconfig.json"),
+    restful: ts.createProject("src/restful/tsconfig.json"),
+};
+
+const dist = "packages";
+const source = "src";
+
+const modules = Object.keys(packages);
+
+gulp.task("default", function () {
+    tasks();
+});
+
+modules.forEach(module => {
+    gulp.task(module, () => {
+        return packages[module]
+            .src()
+            .pipe(tslint({
+                formatter: "verbose",
+            }))
+            .pipe(tslint.report({
+                emitError: false,
+                summarizeFailureOutput: true,
+            }))
+            .pipe(packages[module]())
+            .pipe(gulp.dest(`${dist}/${module}`));
+    });
+});
+
+gulp.task("build", function (cb) {
+    sequence("core", modules.filter((module) => module !== "core"), cb);
+});
+
+function tasks() {
+    modules.forEach(module => {
+        watchTypescript(source, module);
+    });
+}
+
+function watchTypescript(source, module) {
+    gulp.watch(
+        [
+            `${source}/${module}/**/*.ts`,
+            `${source}/${module}/**/*.tsx`,
+            `${source}/${module}/*.ts`,
+            `${source}/${module}/*.tsx`,
+        ],
+        [
+            module,
+        ]
+    ).on("change", function (event) {
+        console.log("File " + event.path + " was " + event.type + ", running tasks...");
+    });
+}
